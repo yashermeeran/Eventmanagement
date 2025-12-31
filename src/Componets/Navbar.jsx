458075abb6c9ff07948f4619434cc093f24f71ef
yasher0304 <yasher0304@gmail.com>
2025-12-31 13:39:13 +0530

    Responce view

--- src/Componets/Navbar.jsx
+++ src/Componets/Navbar.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="nav-left">
-        <img src="./logo.png" alt="Logo" className="nav-logo" />
+        <img src="./logo.png" alt="Logo" />
       </div>
 
-      <div className="nav-right">
-        <Link to="/home">HOME</Link>
-        <Link to="/about">ABOUT</Link>
-        <Link to="/decoration">DECORATION</Link>
-        <Link to="/services">SERVICES</Link>
-        <Link to="/contact">CONTACT</Link>
+      <div className={open ? "nav-right active" : "nav-right"}>
+        <Link to="/home" onClick={()=>setOpen(false)}>HOME</Link>
+        <Link to="/about" onClick={()=>setOpen(false)}>ABOUT</Link>
+        <Link to="/decoration" onClick={()=>setOpen(false)}>DECORATION</Link>
+        <Link to="/services" onClick={()=>setOpen(false)}>SERVICES</Link>
+        <Link to="/contact" onClick={()=>setOpen(false)}>CONTACT</Link>
       </div>
+
+      <i
+        className="fa-solid fa-bars menu-icon"
+        onClick={() => setOpen(!open)}
+      ></i>
     </nav>
   );
 }
